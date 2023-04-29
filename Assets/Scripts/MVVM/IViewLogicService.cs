6f6f6a294a8bbed56997b394e772292a42caeae7
Igor Budarin <jinnfighterwork@yandex.ru>
2023-04-29 03:36:50 +0300

    add: view logic creation logic

--- Assets/Scripts/MVVM/IViewLogicService.cs
+++ Assets/Scripts/MVVM/IViewLogicService.cs
@@ -3,5 +3,8 @@ namespace MVVM
     public interface IViewLogicService
     {
         ViewFactory ViewFactory { get; }
+
+        TViewLogic CreateViewLogic<TViewLogic, TView>(IViewModel viewModel, string viewKey) where TViewLogic : BaseViewLogic where TView : View;
+        TViewLogic CreateViewLogic<TViewLogic, TView>(IViewModel viewModel, TView view) where TViewLogic : BaseViewLogic where TView : View;
     }
 }
--- Assets/Scripts/MVVM/ViewLogicService.cs
+++ Assets/Scripts/MVVM/ViewLogicService.cs
@@ -1,4 +1,6 @@
-﻿namespace MVVM
+﻿using System;
+
+namespace MVVM
 {
     public class ViewLogicService : IViewLogicService
     {
@@ -8,5 +10,19 @@
         }
 
         public ViewFactory ViewFactory { get; }
+
+        public TViewLogic CreateViewLogic<TViewLogic, TView>(IViewModel viewModel, string viewKey)
+            where TViewLogic : BaseViewLogic where TView : View
+        {
+            return Activator.CreateInstance(typeof(TViewLogic), viewModel, ViewFactory.GetView<TView>(viewKey)) as
+                TViewLogic;
+        }
+
+        public TViewLogic CreateViewLogic<TViewLogic, TView>(IViewModel viewModel, TView view)
+            where TViewLogic : BaseViewLogic where TView : View
+        {
+            return Activator.CreateInstance(typeof(TViewLogic), viewModel, view) as
+                TViewLogic;
+        }
     }
 }
